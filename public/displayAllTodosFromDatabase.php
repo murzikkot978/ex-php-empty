487445b7a93714efaae7e9c52944a6301095cd64
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-04-07 10:42:44 +0200

    feat: displayAllTodosFromDatabase is finished

--- public/displayAllTodosFromDatabase.php
+++ public/displayAllTodosFromDatabase.php
@@ -9,6 +9,35 @@
  * The sort option selected must be remembered after the form submission (use a query parameter).
  * The todo title and date should be displayed in a list (date in american format).
  */
+$errors = [];
+$sort = filter_input(INPUT_GET, 'sort');
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+
+    if (count($errors) === 0) {
+        $dsn = 'sqlite:../database.db';
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+
+        try {
+            $todos = [];
+            $pdo = new PDO($dsn, null, null, $options);
+            if ($sort === "name") {
+                $stmt = $pdo->query('SELECT * FROM todos ORDER BY title');
+            } elseif ($sort === "date") {
+                $stmt = $pdo->query('SELECT * FROM todos ORDER BY due_date');
+            } else {
+                $stmt = $pdo->query('SELECT * FROM todos');
+            }
+            $todos = $stmt->fetchAll();
+        } catch (Exception $e) {
+            array_push($errors, $e->getMessage());
+        }
+    }
+}
+
 
 ?>
 
@@ -31,5 +60,34 @@
 
 <!-- WRITE YOUR HTML AND PHP TEMPLATING HERE -->
 
+<form $todos=[]; method="get">
+    <select name="sort" id="">
+        <option value="base" ; ?>Basic</option>
+        <option value="name" ; ?>Name</option>
+        <option value="date" ; ?>Date</option>
+    </select>
+    <input value="Select sort type" type="submit">
+</form>
+
+<?php if (count($errors) > 0): ?>
+    <ul>
+        <?php foreach ($errors as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach ?>
+    </ul>
+<?php endif ?>
+
+<ul>
+    <?php foreach ($todos as $todo): ?>
+        <li>
+            <?= $todo['title'] ?><br/>
+            <time><?= $todo['due_date'] ?></time>
+            <form action="deleteTodoFromDatabase.php" method="post">
+                <input type="hidden" name="idForDelete" value=<?= $todo['id'] ?>>
+                <button type="submit">Delete</button>
+            </form>
+        </li>
+    <?php endforeach ?>
+</ul>
 </body>
 </html>
