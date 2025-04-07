8e1113007e924555b3e883db8a8bfe644cf620f0
murzikkot978 <Roman.Yanivets@jobtrek.ch>
2025-04-07 10:41:57 +0200

    feat: writeTodoToDatabase is finished

--- public/writeTodoToDatabase.php
+++ public/writeTodoToDatabase.php
@@ -9,6 +9,52 @@
  * table `todos` columns `title` and `due_date`. Then redirect the user to the list of todos.
  * If the user input is invalid, display an error to the user
  */
+$errors = [];
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $name = filter_input(INPUT_POST, 'name');
+    $date = filter_input(INPUT_POST, 'date');
+
+    if ($date === "") {
+        array_push($errors, "Date is not a valid date");
+    }
+
+
+    if ($name === "") {
+        array_push($errors, "Name is not a valid name");
+    }
+
+
+    if (count($errors) === 0) {
+        $dsn = 'sqlite:../database.db';
+        $user = 'root';
+        $pass = '';
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        try {
+            $pdo = new PDO($dsn, $user, $pass, $options);
+            $stmt = $pdo->prepare("INSERT INTO todos (title, due_date) VALUES (:name, :date)");
+            $stmt->execute(["name" => $name, "date" => $date]);
+            header("Location: displayAllTodosFromDatabase.php");
+            exit();
+        } catch (Exception $e) {
+            var_dump($e);
+            array_push($errors, $e->getMessage());
+        }
+    }
+}
+
+
+function displayTodos($name, $date)
+{
+    if ($name && $date) {
+        return "$name  $date";
+    }
+}
+
 
 ?>
 
@@ -26,6 +72,28 @@
 <h1>
     Create a new todo
 </h1>
+
+<?php if (count($errors) > 0): ?>
+    <ul>
+        <?php foreach ($errors as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach ?>
+    </ul>
+<?php endif ?>
+
+<form method="post" action="writeTodoToDatabase.php" ?>
+    <label for="title">Name</label>
+    <input type="text" name="name" id="title" value="<?= $name ?>">
+    <label for="date">Date</label>
+    <input type="date" name="date" id="date" value="<?= $date ?>">
+    <button type="submit">submit</button>
+</form>
+
+<ul>
+    <li>
+        <?= displayTodos($name, $date) ?>
+    </li>
+</ul>
 <!-- WRITE YOUR HTML AND PHP TEMPLATING HERE -->
 </body>
 </html>
